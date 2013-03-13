438f0d38756978b01ff7b5c7a67fc564d401a6bd
Asa Jay <hifreeo@gmail.com>
2013-03-14 00:48:21 +0100

    Alpha 2.2.0.1
    Commit error, settings.py not comitted.

--- root/djVacs/settings.py
+++ root/djVacs/settings.py
@@ -124,7 +124,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -194,9 +194,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     "south",
-    #"polls",
     "eyevacs",
-    # 'debug_toolbar',
+    'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
